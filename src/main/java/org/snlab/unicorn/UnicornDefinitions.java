a8cc526958fa6c0cdc996bf84219e64b43e64d96
Y.Jace Liu <yang.jace.liu@linux.com>
2017-10-31 18:10:49 +0800

    Remove hard-coded domain info
    Signed-off-by: Y.Jace Liu <yang.jace.liu@linux.com>

--- src/main/java/org/snlab/unicorn/UnicornDefinitions.java
+++ src/main/java/org/snlab/unicorn/UnicornDefinitions.java
@@ -4,9 +4,7 @@ public class UnicornDefinitions {
     public static class OrchestratorConfig {
         public static String CONFIG_PATH = "/orchestrator/orchestrators.json";
     }
-
-    public static String SERVER_NAME = "DOMAIN_1";
-    public static String SERVER_IP = "10.0.0.1";
-    public static String SERVER_PORT = "8181";
-    public static String CONTROL_URL = "http://" + SERVER_IP + ":" + SERVER_PORT + "/experimental/v1/control";
+    public static class ServerConfig{
+        public static String CONFIG_PATH = "/server.json";
+    }
 }
