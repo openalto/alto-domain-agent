4f5a7afd0bdc850c7f894ddbe7428aedc2e16e32
Y.Jace Liu <yang.jace.liu@linux.com>
2017-11-02 11:52:59 +0800

    Complete registration code
    Signed-off-by: Y.Jace Liu <yang.jace.liu@linux.com>

--- src/main/java/org/snlab/unicorn/register/UnicornRegisterImpl.java
+++ src/main/java/org/snlab/unicorn/register/UnicornRegisterImpl.java
@@ -7,6 +7,12 @@ import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonBuilderFactory;
 import javax.json.JsonObject;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.Collection;
 import java.util.HashMap;
 
@@ -17,9 +23,7 @@ public class UnicornRegisterImpl implements UnicornRegister {
         this.infos = infos;
     }
 
-    @Override
-    public void register() {
-        // Build register json
+    private JsonObject buildRegisterJSON() {
         ServerInfo serverInfo = ServerInfo.getInstance();
         JsonBuilderFactory factory = Json.createBuilderFactory(new HashMap<>());
         JsonArrayBuilder hostsBuilder = factory.createArrayBuilder();
@@ -37,11 +41,50 @@ public class UnicornRegisterImpl implements UnicornRegister {
                 .add("hosts", hostsBuilder)
                 .add("ingress-points", ingressPointsBuilder)
                 .build();
+        return object;
+    }
 
-        // Send the register object to remote
-        for (OrchestratorInfo info: this.infos){
+    @Override
+    public void register() {
+        // Build register json
+        JsonObject object = buildRegisterJSON();
+
+        // Send the register json to every domain
+        for (OrchestratorInfo info : this.infos) {
             String remoteURL = "http://" + info.getIp() + ":" + info.getPort() + info.getRegistry();
-            //TODO: send to remote
+            try {
+                URL url = new URL(remoteURL);
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+                // Headers Setting
+                connection.setRequestMethod("POST");
+                connection.setRequestProperty("Content-Type", "application/json");
+                connection.setRequestProperty("Content-Length", Integer.toString(object.toString().getBytes().length));
+                connection.setRequestProperty("Content-Language", "en-US");
+                connection.setUseCaches(false);
+                connection.setDoOutput(true);
+
+                // Send Request
+                DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+                wr.writeBytes(object.toString());
+                wr.close();
+
+                // Get Response
+                InputStream is = connection.getInputStream();
+                BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+                StringBuilder response = new StringBuilder();
+                String line;
+                while((line = rd.readLine()) != null){
+                    response.append(line);
+                    response.append('\r');
+                }
+                rd.close();
+                String responseString = response.toString();
+
+                // TODO: check response string
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 }
