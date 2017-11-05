4b814d676f46c1e370c6ccac54fd97cc86b9612e
jensenzhang <jingxuan.n.zhang@gmail.com>
2017-11-05 01:24:47 -0400

    Fix #3: Delay boostrap stage
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

---
+++ src/main/java/org/snlab/unicorn/Bootstrap.java
@@ -0,0 +1,27 @@
+package org.snlab.unicorn;
+
+import java.util.Collection;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.snlab.unicorn.orchestrator.OrchestratorInfo;
+import org.snlab.unicorn.orchestrator.OrchestratorInfoReader;
+import org.snlab.unicorn.register.UnicornRegister;
+import org.snlab.unicorn.register.UnicornRegisterBuilder;
+
+public class Bootstrap extends HttpServlet {
+    private final static Logger LOG = LoggerFactory.getLogger(Bootstrap.class);
+
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        LOG.info("Starting bootstrap stage...");
+        // Register to orchestrator
+        Collection<OrchestratorInfo> infos = OrchestratorInfoReader.read(UnicornDefinitions.OrchestratorConfig.CONFIG_PATH);
+        UnicornRegister register = new UnicornRegisterBuilder().addOrchestrators(infos).build();
+        register.register();
+    }
+}
--- src/main/java/org/snlab/unicorn/UnicornApplication.java
+++ src/main/java/org/snlab/unicorn/UnicornApplication.java
@@ -2,20 +2,14 @@ package org.snlab.unicorn;
 
 import org.glassfish.jersey.media.sse.SseFeature;
 import org.glassfish.jersey.server.ResourceConfig;
-import org.snlab.unicorn.orchestrator.OrchestratorInfo;
-import org.snlab.unicorn.orchestrator.OrchestratorInfoReader;
-import org.snlab.unicorn.register.UnicornRegister;
-import org.snlab.unicorn.register.UnicornRegisterBuilder;
-
-import java.util.Collection;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class UnicornApplication extends ResourceConfig {
+    private final static Logger LOG = LoggerFactory.getLogger(UnicornApplication.class);
+
     public UnicornApplication() {
         super(UnicornService.class, SseFeature.class);
-
-        // Register to orchestrator
-        Collection<OrchestratorInfo> infos = OrchestratorInfoReader.read(UnicornDefinitions.OrchestratorConfig.CONFIG_PATH);
-        UnicornRegister register = new UnicornRegisterBuilder().addOrchestrators(infos).build();
-        register.register();
+        LOG.info("Starting unicorn application...");
     }
 }
