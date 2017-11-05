4c6e9612ae0d5edf9f92a6cba3043e41fe3cc2cc
Y.Jace Liu <yang.jace.liu@linux.com>
2017-11-05 12:51:53 +0800

    Setting control stream route in a right way
    Signed-off-by: Y.Jace Liu <yang.jace.liu@linux.com>

--- src/main/java/org/snlab/unicorn/UnicornService.java
+++ src/main/java/org/snlab/unicorn/UnicornService.java
@@ -1,15 +1,17 @@
 package org.snlab.unicorn;
 
-import java.util.UUID;
-
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.sse.Sse;
 import javax.ws.rs.sse.SseEventSink;
+import java.nio.file.Paths;
+import java.util.UUID;
 
 /**
  * Root resource (exposed at "unicorn" path)
@@ -18,6 +20,8 @@ import javax.ws.rs.sse.SseEventSink;
 public class UnicornService {
 
     private final static String UPDATES_STREAM_CONTROL_TYPE = "application/updatestreamcontrol";
+    private final static String UPDATE_STREAM_ROUTE = "updates";
+    private final static String CONTROL_STREAM_ROUTE = "controls";
 
     @Context
     private UriInfo uriInfo;
@@ -35,16 +39,30 @@ public class UnicornService {
     }
 
     @GET
-    @Path("updates")
+    @Path(UPDATE_STREAM_ROUTE)
     @Produces(MediaType.SERVER_SENT_EVENTS)
     public void establishUpdateStream(@Context SseEventSink eventSink, @Context Sse sse) {
         UUID newControlStreamId = UUID.randomUUID();
         eventSink.send(sse.newEventBuilder()
                 .name(UPDATES_STREAM_CONTROL_TYPE)
-                .data(uriInfo.getBaseUri() + "/" + newControlStreamId)
+                .data(uriInfo.getBaseUri().toString() + Paths.get("unicorn", CONTROL_STREAM_ROUTE, newControlStreamId.toString()).toString())
                 .build());
         // TODO: Track update stream and handle the following dynamic updates
         new Thread(() -> {
         }).start();
+    }
+
+    @Path(CONTROL_STREAM_ROUTE)
+    public ControlServiceResource controlServiceResource() {
+        return new ControlServiceResource();
+    }
+
+    public class ControlServiceResource {
+        @POST
+        @Path("{id}")
+        public void getControlStreamQuery(@PathParam("id") String id) {
+            System.out.println(id);
+            //TODO
+        }
     }
 }
