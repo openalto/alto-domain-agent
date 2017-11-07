fcdaea668df7062e0873e02fc7b1101fd263131d
jensenzhang <jingxuan.n.zhang@gmail.com>
2017-11-07 01:30:02 -0500

    Add a timer based dynamic update for mock adapter
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- src/main/java/org/snlab/unicorn/adapter/MockAdapter.java
+++ src/main/java/org/snlab/unicorn/adapter/MockAdapter.java
@@ -5,6 +5,7 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 import org.snlab.unicorn.model.Ane;
 import org.snlab.unicorn.model.AneMatrix;
@@ -18,6 +19,20 @@ public class MockAdapter implements ControllerAdapter {
     private final static String DEFAULT_IPV4_ADDR = "10.10.10.10";
     private final static int MAX_ANE_NUM = 10;
     private Random randomGen = new Random();
+    private boolean changeSignal = false;
+
+    public MockAdapter() {
+        new Thread(() -> {
+            while (true) {
+                try {
+                    TimeUnit.MINUTES.sleep(1L);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+                changeSignal = true;
+            }
+        }).start();
+    }
 
     @Override
     public PathQueryResponseBody getAsPath(List<QueryItem> querySet) {
@@ -27,7 +42,7 @@ public class MockAdapter implements ControllerAdapter {
             try {
                 byte[] address = new byte[4];
                 randomGen.nextBytes(address);
-				response.add(Inet4Address.getByAddress(address).toString());
+				response.add(Inet4Address.getByAddress(address).getHostAddress());
 			} catch (UnknownHostException e) {
                 response.add(DEFAULT_IPV4_ADDR);
 			}
@@ -46,7 +61,7 @@ public class MockAdapter implements ControllerAdapter {
         int aneNum = randomGen.nextInt(MAX_ANE_NUM) + 1;
         for (int i = 0; i < aneNum; i++) {
             Ane ane = new Ane();
-            ane.setAvailbw(randomGen.nextLong());
+            ane.setAvailbw(randomGen.nextInt(100000000) * 1000L);
             anes.add(ane);
             List<AneMatrix> matrixRow = new ArrayList<>();
             for (QueryItem item : querySet) {
@@ -65,12 +80,16 @@ public class MockAdapter implements ControllerAdapter {
 
     @Override
     public boolean ifAsPathChangedThenCleanState() {
+        if (changeSignal == true) {
+            changeSignal = false;
+            return true;
+        }
         return false;
     }
 
     @Override
     public boolean ifResourceChangedThenCleanState() {
-        return false;
+        return ifAsPathChangedThenCleanState();
     }
 
 }
