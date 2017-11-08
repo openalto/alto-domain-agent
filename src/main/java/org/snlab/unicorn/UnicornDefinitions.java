5fe63774e9d8b76493c081b851a6df18d400fdef
Y.Jace Liu <yang.jace.liu@linux.com>
2017-11-08 16:36:19 +0800

    Add a mock adapter reading json files
    Signed-off-by: Y.Jace Liu <yang.jace.liu@linux.com>

--- src/main/java/org/snlab/unicorn/UnicornDefinitions.java
+++ src/main/java/org/snlab/unicorn/UnicornDefinitions.java
@@ -4,7 +4,12 @@ public class UnicornDefinitions {
     public static class OrchestratorConfig {
         public static String CONFIG_PATH = "/orchestrator/orchestrators.json";
     }
-    public static class ServerConfig{
+
+    public static class ServerConfig {
         public static String CONFIG_PATH = "/server.json";
+    }
+
+    public static class AdapterConfig {
+        public static String MOCK_CONFIG_PATH = "/adapter/mock.json";
     }
 }
