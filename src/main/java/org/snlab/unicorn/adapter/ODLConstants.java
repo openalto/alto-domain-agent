11f91d8d45cd7d027259ff2a360b960c2a1d0c10
jensenzhang <jingxuan.n.zhang@gmail.com>
2017-11-13 17:13:36 -0700

    Fix #26: Change websocket stream to configuration datastore
    Signed-off-by: jensenzhang <jingxuan.n.zhang@gmail.com>

--- src/main/java/org/snlab/unicorn/adapter/ODLConstants.java
+++ src/main/java/org/snlab/unicorn/adapter/ODLConstants.java
@@ -5,6 +5,6 @@ public class ODLConstants {
     public final static String SAL_REMOTE_SUBSCRIPTION = "/restconf/operations/sal-remote:create-data-change-event-subscription";
     public final static String RESTCONF_STREAM = "/restconf/streams/stream/";
     public final static String DEFAULT_NOTIFICATION_SUBSCRIPTION = "{\"input\":{\"path\":\"/opendaylight-inventory:nodes\",\"datastore\":\"OPERATIONAL\",\"scope\":\"SUBTREE\"}}";
-    public final static String UNICORN_PATH_MANAGER_SUBSCRIPTION = "{\"input\":{\"path\":\"/alto-pathmanager:path-manager\",\"datastore\":\"OPERATIONAL\",\"scope\":\"SUBTREE\"}}";
+    public final static String UNICORN_PATH_MANAGER_SUBSCRIPTION = "{\"input\":{\"path\":\"/alto-pathmanager:path-manager\",\"datastore\":\"CONFIGURATION\",\"scope\":\"SUBTREE\"}}";
     public final static String UNICORN_BANDWIDTH_MONITOR_SUBSCRIPTION = "{\"input\":{\"path\":\"/alto-bwmonitor:speeds\",\"datastore\":\"OPERATIONAL\",\"scope\":\"SUBTREE\"}}";
 }
